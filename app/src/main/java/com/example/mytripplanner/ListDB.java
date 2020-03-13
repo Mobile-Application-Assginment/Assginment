a47a8dd7fe8866ec055c4aa98cefcd44de491aeb
younchul <yccho8212@gmail.com>
2020-03-13 17:12:59 -0400

    adding the comment

--- app/src/main/java/com/example/mytripplanner/ListDB.java
+++ app/src/main/java/com/example/mytripplanner/ListDB.java
@@ -1,6 +1,14 @@
-package com.example.mytripplanner;
+/*
+ *   NAME    : ListDB.java
+ *   Project: Mobile Application Development - Assignment 2
+ *   By: Charng Gwon Lee, Hyungbum Kim, Younchul Cho
+ *   Date: Mar. 14, 2020
+ *   PURPOSE : This file is connect with the database. It defines the database and table.
+ *             Also, it gives inserting and searching database method.
+ *             In this program, it has four tables in a database (airport, user, schedule, and trip task)
+ */
 
-import java.util.ArrayList;
+package com.example.mytripplanner;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -10,71 +18,66 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+
 public class ListDB {
 
     // database constants
-    public static final String DB_NAME = "trip_db.db";
-    public static final int    DB_VERSION = 1;
+    private static final String DB_NAME = "trip_db.db";
+    private static final int    DB_VERSION = 1;
 
 
-    // list table constants
-    public static final String AIRPORT_TABLE = "airport";
-
-    public static final String AIRPORT_ID = "_id";
-    public static final int    AIRPORT_ID_COL = 0;
-
-    public static final String AIRPORT_NAME = "airport_name";
-    public static final int    AIRPORT_NAME_COL = 1;
+    // Airport table constants
+    private static final String AIRPORT_TABLE = "airport";
+    private static final String AIRPORT_ID = "_id";
+    private static final int    AIRPORT_ID_COL = 0;
+    private static final String AIRPORT_NAME = "airport_name";
+    private static final int    AIRPORT_NAME_COL = 1;
 
 
-    // list table constants
-    public static final String TIME_TABLE = "time";
-
-    public static final String TIME_ID = "_id";
-    public static final int    TIME_ID_COL = 0;
-
-    public static final String TIME_VALUE = "time_value";
-    public static final int    TIME_VALUE_COL = 1;
+    // Schedule time table constants
+    private static final String TIME_TABLE = "time";
+    private static final String TIME_ID = "_id";
+    private static final int    TIME_ID_COL = 0;
+    private static final String TIME_VALUE = "time_value";
+    private static final int    TIME_VALUE_COL = 1;
 
 
-    // list table constants
-    public static final String USER_TABLE = "user";
+    // User table constants
+    private static final String USER_TABLE = "user";
+    private static final String USER_ID = "_id";
+    private static final int    USER_ID_COL = 0;
+    private static final String USER_NAME = "user_name";
+    private static final int    USER_NAME_COL = 1;
 
-    public static final String USER_ID = "_id";
-    public static final int    USER_ID_COL = 0;
-
-    public static final String USER_NAME = "user_name";
-    public static final int    USER_NAME_COL = 1;
-
-
-    public static final String TASK_TABLE = "task";
-    public static final String TASK_ID = "_id";
-    public static final int    TASK_ID_COL = 0;
-    public static final String TASK_USER_ID = "user_id";
-    public static final int    TASK_USER_ID_COL = 1;
-    public static final String TASK_AIRPORT_ID = "airport_id";
-    public static final int    TASK_AIRPORT_ID_COL = 2;
-    public static final String TASK_TIME_ID = "time_id";
-    public static final int    TASK_TIME_ID_COL = 3;
+    // Trip task table constants
+    private static final String TASK_TABLE = "task";
+    private static final String TASK_ID = "_id";
+    private static final int    TASK_ID_COL = 0;
+    private static final String TASK_USER_ID = "user_id";
+    private static final int    TASK_USER_ID_COL = 1;
+    private static final String TASK_AIRPORT_ID = "airport_id";
+    private static final int    TASK_AIRPORT_ID_COL = 2;
+    private static final String TASK_TIME_ID = "time_id";
+    private static final int    TASK_TIME_ID_COL = 3;
 
 
-    // CREATE and DROP TABLE statements
-    public static final String CREATE_AIRPORT_TABLE =
+    // CREATE TABLE statements
+    private static final String CREATE_AIRPORT_TABLE =
             "CREATE TABLE " + AIRPORT_TABLE + " (" +
                     AIRPORT_ID   + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     AIRPORT_NAME + " TEXT    NOT NULL UNIQUE);";
 
-    public static final String CREATE_TIME_TABLE =
+    private static final String CREATE_TIME_TABLE =
             "CREATE TABLE " + TIME_TABLE + " (" +
                     TIME_ID   + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     TIME_VALUE + " TEXT    NOT NULL UNIQUE);";
 
-    public static final String CREATE_USER_TABLE =
+    private static final String CREATE_USER_TABLE =
             "CREATE TABLE " + USER_TABLE + " (" +
                     USER_ID   + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     USER_NAME + " TEXT    NOT NULL UNIQUE);";
 
-    public static final String CREATE_TASK_TABLE =
+    private static final String CREATE_TASK_TABLE =
             "CREATE TABLE " + TASK_TABLE + " (" +
                     TASK_ID   + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     TASK_USER_ID         + " INTEGER NOT NULL, " +
@@ -82,20 +85,21 @@ public class ListDB {
                     TASK_TIME_ID         + " INTEGER NOT NULL );";
 
 
-
-    public static final String DROP_AIRPORT_TABLE =
+    // DROP TABLE statements
+    private static final String DROP_AIRPORT_TABLE =
             "DROP TABLE IF EXISTS " + AIRPORT_TABLE;
 
-    public static final String DROP_TIME_TABLE =
+    private static final String DROP_TIME_TABLE =
             "DROP TABLE IF EXISTS " + TIME_TABLE;
 
-    public static final String DROP_USER_TABLE =
+    private static final String DROP_USER_TABLE =
             "DROP TABLE IF EXISTS " + USER_TABLE;
 
-    public static final String DROP_TASK_TABLE =
+    private static final String DROP_TASK_TABLE =
             "DROP TABLE IF EXISTS " + TASK_TABLE;
 
 
+    // Initiate the database (create or drop tables)
     private static class DBHelper extends SQLiteOpenHelper {
 
         public DBHelper(Context context, String name,
@@ -103,33 +107,39 @@ public class ListDB {
             super(context, name, factory, version);
         }
 
+        // Create the each tables which is in the database
         @Override
         public void onCreate(SQLiteDatabase db) {
-            // create tables
+
+            // Create the Table
             db.execSQL(CREATE_AIRPORT_TABLE);
             db.execSQL(CREATE_TIME_TABLE);
             db.execSQL(CREATE_USER_TABLE);
             db.execSQL(CREATE_TASK_TABLE);
 
-            // insert default lists
-            db.execSQL("INSERT INTO airport VALUES (1, 'Toronto')");
-            db.execSQL("INSERT INTO airport VALUES (2, 'London')");
+            // Print log fo successfully create the database
+            Log.d("List DB", "Create Database, the version is " + DB_VERSION);
 
             // insert default lists
-            db.execSQL("INSERT INTO time VALUES (1, '10:00')");
-            db.execSQL("INSERT INTO time VALUES (2, '10:30')");
-
-            // insert default lists
-            db.execSQL("INSERT INTO user VALUES (1, 'Jone')");
-            db.execSQL("INSERT INTO user VALUES (2, 'Doe')");
+//            db.execSQL("INSERT INTO airport VALUES (1, 'Toronto')");
+//            db.execSQL("INSERT INTO airport VALUES (2, 'London')");
+//
+//            // insert default lists
+//            db.execSQL("INSERT INTO time VALUES (1, '10:00')");
+//            db.execSQL("INSERT INTO time VALUES (2, '10:30')");
+//
+//            // insert default lists
+//            db.execSQL("INSERT INTO user VALUES (1, 'Jone')");
+//            db.execSQL("INSERT INTO user VALUES (2, 'Doe')");
         }
 
+
+        // Upgrade the database (Remove/drop the all table and create again)
         @Override
         public void onUpgrade(SQLiteDatabase db,
                               int oldVersion, int newVersion) {
 
-            Log.d("List DB", "Upgrading db from version "
-                    + oldVersion + " to " + newVersion);
+            Log.d("List DB", "Upgrading db from version " + oldVersion + " to " + newVersion);
 
             db.execSQL(ListDB.DROP_AIRPORT_TABLE);
             db.execSQL(ListDB.DROP_TIME_TABLE);
@@ -140,35 +150,39 @@ public class ListDB {
     }
 
 
-    // database and database helper objects
+
+
+    // Database and Database helper objects
     private SQLiteDatabase db;
     private DBHelper dbHelper;
+
 
     // constructor
     public ListDB(Context context) {
         dbHelper = new DBHelper(context, DB_NAME, null, DB_VERSION);
     }
 
-
     // private methods
     private void openReadableDB() {
         db = dbHelper.getReadableDatabase();
     }
-
     private void openWriteableDB() {
         db = dbHelper.getWritableDatabase();
     }
 
+    // Close the database, if it is opened
     private void closeDB() {
         if (db != null)
             db.close();
     }
 
+    // Close the cursor, if it is opened
     private void closeCursor(Cursor cursor) {
         if (cursor != null)
             cursor.close();
     }
 
+    // Insert the new name of airport into the database
     public long insertAirport(String name) {
         ContentValues cv = new ContentValues();
         cv.put(AIRPORT_NAME, name);
@@ -180,6 +194,7 @@ public class ListDB {
         return rowID;
     }
 
+    // Insert the new name of user into the database
     public long insertUser(String name) {
         ContentValues cv = new ContentValues();
         cv.put(USER_NAME, name);
@@ -191,6 +206,8 @@ public class ListDB {
         return rowID;
     }
 
+
+    // Insert the new schedule time into the database
     public long insertTime(String time) {
         ContentValues cv = new ContentValues();
         cv.put(TIME_VALUE, time);
@@ -202,6 +219,7 @@ public class ListDB {
         return rowID;
     }
 
+    // Insert the new trip task into the database
     public long insertTask(Task task) {
         ContentValues cv = new ContentValues();
         cv.put(TASK_AIRPORT_ID, task.getAirportId());
@@ -215,7 +233,7 @@ public class ListDB {
         return rowID;
     }
 
-
+    // Get the airport id based on the name of airport
     public long getAirportId(String name) {
         String where = AIRPORT_NAME + "= ?";
         String[] whereArgs = { name };
@@ -234,6 +252,7 @@ public class ListDB {
         else { return id; }
     }
 
+    // Get the airport name based on the airport id
     public String getAirportName(int id) {
         String where = AIRPORT_ID + "= ?";
         String[] whereArgs = { Integer.toString(id) };
@@ -251,6 +270,8 @@ public class ListDB {
         else { return null; }
     }
 
+
+    // Get the whole airport information which cursor indicate
     private static Airport getAirportFromCursor(Cursor cursor) {
         if (cursor == null || cursor.getCount() == 0){
             return null;
@@ -269,7 +290,7 @@ public class ListDB {
         }
     }
 
-
+    // Get the user id based on the name of the user
     public long getUserId(String name) {
         String where = USER_NAME + "= ?";
         String[] whereArgs = { name };
@@ -290,6 +311,8 @@ public class ListDB {
         else { return id; }
     }
 
+
+    // Get the user name based on the user id
     public String getUserName(int id) {
         String where = USER_ID + "= ?";
         String[] whereArgs = { Integer.toString(id) };
@@ -307,6 +330,7 @@ public class ListDB {
         else { return null; }
     }
 
+    // Get the whole user information which cursor indicate
     private static User getUserFromCursor(Cursor cursor) {
         if (cursor == null || cursor.getCount() == 0){
             return null;
@@ -326,6 +350,7 @@ public class ListDB {
     }
 
 
+    // Get the specific time id based on schedule time
     public long getTimeId(String time) {
         String where = TIME_VALUE + "= ?";
         String[] whereArgs = { time };
@@ -346,6 +371,7 @@ public class ListDB {
         else { return id; }
     }
 
+    // Get the specific time based on time id
     public String getTimeValue(int id) {
         String where = TIME_ID + "= ?";
         String[] whereArgs = { Integer.toString(id) };
@@ -363,6 +389,7 @@ public class ListDB {
         else { return null; }
     }
 
+    // Get the whole schedule time information which cursor indicate
     private static ScheduleTime getTimeFromCursor(Cursor cursor) {
         if (cursor == null || cursor.getCount() == 0){
             return null;
--- app/src/main/java/com/example/mytripplanner/User.java
+++ app/src/main/java/com/example/mytripplanner/User.java
@@ -1,21 +1,35 @@
+/*
+ *   NAME    : User.java
+ *   Project: Mobile Application Development - Assignment 2
+ *   By: Charng Gwon Lee, Hyungbum Kim, Younchul Cho
+ *   Date: Mar. 14, 2020
+ *   PURPOSE : The Task class contain the user information in the database structure.
+ *             It will store the information and return to that it asked.
+ */
+
 package com.example.mytripplanner;
 
 public class User {
 
+    // The properties
     private long userId;
     private String userName;
 
 
+    // The constructor which has two argument for each properties
     public User (int id, String name) {
         this.userId = id;
         this.userName = name;
     }
 
+
+    // Get method for each properties. It will return the value of property
     public long getUserId() { return userId; }
+    public String getUserName() { return userName; }
+
+    // Set method for each properties. It will update the properties information
     public void setUserId(long id) {
         this.userId = id;
     }
-
-    public String getUserName() { return userName; }
     public void setUserName(String name) { this.userName = name; }
 }
