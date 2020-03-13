86705ed21e3e4bdc4f66c6cb836e976ea7df57fd
Hyungbum Kim <coolish46@gmail.com>
2020-03-13 16:27:17 -0400

    Add FlightInfoResult.java

---
+++ app/src/main/java/com/example/mytripplanner/FlightInfoResult.java
@@ -0,0 +1,10 @@
+package com.example.mytripplanner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class FlightInfoResult {
+    private ArrayList<HashMap<String,String>> data;
+    public  ArrayList<HashMap<String,String>> getData(){return data;}
+    public  void setData(ArrayList<HashMap<String,String>>data){this.data = data;}
+}
