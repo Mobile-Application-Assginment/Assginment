e23ee2a86afedb2e8be6c49d83e7c7e146034af4
Charng Gwon Lee <clee9809@conestogac.on.ca>
2020-03-14 18:45:16 -0400

    add comments

--- app/src/main/java/com/example/mytripplanner/RequestItemServiceTask.java
+++ app/src/main/java/com/example/mytripplanner/RequestItemServiceTask.java
@@ -22,27 +22,22 @@ import java.util.HashMap;
 public class RequestItemServiceTask extends AsyncTask<Void,Void,String> {
 
     private Context mContext = null;
-    public static String strTTS; //cgl
+    public static String strTTS;                                    // for Text-to-Speech
     public  RequestItemServiceTask(Context context){this.mContext = context;}
 
     @Override
     protected String doInBackground(Void... unused) {
         Log.i("MyTripPlanner","In Call service");
         try{
-            Log.i("TTS","downloading json file for TTS");  //cgl
-            JSONArray serviceItems2 = WebServiceUtil.requestWebService("http://10.0.2.2:3001/root").getJSONArray("TTS"); //cgl
-            JSONObject obj2 = serviceItems2.getJSONObject(0); //cgl
-            strTTS = (String)obj2.getString("welcome"); //cgl
+            Log.i("TTS","downloading json file for TTS");
+            JSONArray serviceItems2 = WebServiceUtil.requestWebService("http://10.0.2.2:3001/root").getJSONArray("TTS");
+            JSONObject obj2 = serviceItems2.getJSONObject(0);
+            strTTS = (String)obj2.getString("welcome");
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return strTTS;
-    }
-
-    @Override
-    protected void onPostExecute(String result) {  //cgl remove
-        ListDB db = new ListDB(mContext); 
     }
 }
