8c251a4c50c163ef46adbc5615a9896ca367ff61
Hyungbum Kim <coolish46@gmail.com>
2020-03-13 21:00:48 -0400

    Remove Json from Asset

--- app/src/main/java/com/example/mytripplanner/FlightInfoResult.java
+++ app/src/main/java/com/example/mytripplanner/FlightInfoResult.java
@@ -6,5 +6,7 @@ import java.util.HashMap;
 public class FlightInfoResult {
     private ArrayList<HashMap<String,String>> data;
     public  ArrayList<HashMap<String,String>> getData(){return data;}
-    public  void setData(ArrayList<HashMap<String,String>>data){this.data = data;}
+    public  void setData(ArrayList<HashMap<String,String>>data){
+        this.data = data;
+    }
 }
