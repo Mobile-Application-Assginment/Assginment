fafa791bec7593a01eaa6c97a27cdbb646229299
Younchul <yccho8212@gmail.com>
2020-04-15 21:05:48 -0400

    Change the comment for map activity.

--- app/src/main/java/com/example/mytripplanner/MapsActivity.java
+++ app/src/main/java/com/example/mytripplanner/MapsActivity.java
@@ -1,3 +1,13 @@
+/*
+ *   NAME    : FlightInfoResult.java
+ *   Project: Mobile Application Development - Assignment 5
+ *   By: Charng Gwon Lee, Hyungbum Kim, Younchul Cho
+ *   Date: April. 17, 2020
+ *   PURPOSE : The MapsActivity class has been created to Google map.
+ *             It is going to set the location of our headquarter (Conestoga college waterloo campus).
+ *             Also, it zooms in to proper scale and show a marker.
+ */
+
 package com.example.mytripplanner;
 
 import androidx.fragment.app.FragmentActivity;
@@ -29,19 +39,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * we just add a marker Conestoga college waterloo campus.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in our headquarter and move the camera
+        // Set the position
         LatLng headquarter = new LatLng(43.479273, -80.518376);
 
+        // Create the marker for the location
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(headquarter);
         markerOptions.title("Conestoga Travel");
